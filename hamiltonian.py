5be17a2f7cb732596baa15dce0b37686cc70e352
Igor Benek-Lins <beneklins@protonmail.ch>
2021-02-04 05:49:57 -0500

    Update licence

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,23 +1,15 @@
-'''
-Hamiltonian.py - Define Hamiltonian
+"""
+hamiltonian.py - Define the Hamiltonian
 
-Copyright 2020-2021 Maxime Dion <maxime.dion@usherbrooke.ca>
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Copyright © 2021 Brett Henderson <brettrhenderson25@gmail.com>,
+                 Igor Benek-Lins <physics@ibeneklins.com>,
+                 Melvin Mathews <mel.matt007@gmail.com>,
+Copyright © 2020-2021 Maxime Dion <maxime.dion@usherbrooke.ca>
 
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-MODIFIED 24-01-2021 by Brett Henderson <brettrhenderson25@gmail.com>:
-    Implemented all methods with "# TO COMPLETE" identifiers.
-'''
+This file is part of “The Three Qiskiteers H₂ ground state finder” (T3QH2).
+For the licence, see the LICENCE file.
+"""
 
 import numpy as np
 from pauli_string import PauliString, LinearCombinaisonPauliString
